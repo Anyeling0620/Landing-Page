fea383a120b0702c522a4938e7d98b31f856af93
Anyeling0620 <3463645195@qq.com>
2026-06-15 20:59:24 +0800

    init: 完成安装配置，初步完成导航栏

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "./components/Layout";
+import { Hero } from "./components/sections/Hero";
+
+
+function App () {
+  return (
+    <Layout title='借贷页面'>
+      <Hero />
+    </Layout>
+  );
+}
+
+export default App;
